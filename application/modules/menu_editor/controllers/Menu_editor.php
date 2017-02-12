5c12632da34a16f5e73b8bff3dbc806763432e5d
Karl Gray <github@gray.me.uk>
2017-02-12 04:08:19 +0000

    Added menu_editor_model with add, save and list functionality, Integrated it into editor

--- application/modules/menu_editor/controllers/Menu_editor.php
+++ application/modules/menu_editor/controllers/Menu_editor.php
@@ -5,8 +5,10 @@ class Menu_editor extends Public_Controller {
 	{
 		parent::__construct();
 		$this->template_module_path = APPPATH.'modules/'.$this->router->fetch_module() . "/views/";
-	// Load the module config file.
-		$this->load->config('config');
+
+		//load the editor model
+		$this->load->model('menu_editor/menu_editor_model');
+		
 	}
 
 	public function menu_edit($menu) {
@@ -16,8 +18,14 @@ class Menu_editor extends Public_Controller {
 	public function index() {
 		$this->template->set_title("Menu Editor.");
 		$menu = array();
-		$page_data = '';
-		$page_data = $this->load->view("menu_editor/menu_editor_view", $menu, TRUE);
+		$editor = New Menu_editor_model;
+		$menu_list = array(
+			'menu_list' => $editor->list_menus()
+			);
+		
+
+		$page_data = $this->parser->parse('menu_editor/menu_editor_view', $menu_list, TRUE);
+		#$page_data = $this->load->view("menu_editor/menu_editor_view", $menu_list, TRUE);
 		$this->template->set_page_data($page_data);
 		$this->template->display_page();
 		
@@ -26,7 +34,8 @@ class Menu_editor extends Public_Controller {
 	public function ajax_load() {
 		if ($this->input->post('menu'))
 		{
-			// Load it here.
+			$editor = New Menu_editor_model;
+			echo ($editor->load_menu($this->input->post('menu')));
 		}
 	}
 
@@ -35,10 +44,17 @@ class Menu_editor extends Public_Controller {
 		if ($this->input->post('menu')) 
 		{
 			$html= $this->input->post('menu');
-			// Save it here.
-			//echo $html;
-			echo "OK";
-			return;
+			$html = str_replace( 'class="ui-sortable"', "", $html);
+			$html = str_replace( 'class="ui-sortable-handle"', "", $html);
+
+			// Get the name of the menu.
+			$dom = new DOMDocument;
+			$dom->loadHTML($html);
+
+			$menu_name = $dom->getElementsByTagName('ul')->item(0)->getAttribute('id');
+			$editor = New Menu_editor_model;
+			$editor->save_menu($menu_name, $html);
+
 
 		}
 		else

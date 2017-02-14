f1934c37214f91200cbd76590036b59dc4bb3dea
Karl Gray <github@gray.me.uk>
2017-02-14 16:33:10 +0000

    added parser to left menu template

--- application/modules/menu/views/default_left_menu_view.php
+++ application/modules/menu/views/default_left_menu_view.php
@@ -27,6 +27,25 @@
 						</ul>
 						<!-- /.nav-second-level -->
 					</li>
+
+					{menu}
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="{href}">
+							{icon} {title}
+						</a>
+						<ul class="{ul_class}">
+							{submenu}
+							<li>
+								<a href="{href}">
+									{icon} {title}
+								</a>
+							</li>
+							{footer}
+							{/submenu}
+						</ul>
+					</li>
+					{/menu}
+					
 				</ul>
 			</div>
 			<!-- /.sidebar-collapse -->

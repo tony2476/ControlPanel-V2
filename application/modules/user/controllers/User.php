6223277b7d0e3b4bd6632aeb106f7b93836360fa
Karl Gray <github@gray.me.uk>
2017-02-15 18:31:14 +0000

    Added buttons to /user/user_list  created toggle_status and login_as_user functionality

--- application/modules/user/controllers/User.php
+++ application/modules/user/controllers/User.php
@@ -1,6 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 
+
+/**
+ *  This class handles functionality related to logged in users.  
+ *  It cannot access any other system or modules except for ion_auth
+ *  ion_auth is a requirement.
+ */
+
 class User extends Public_Controller {
 	private $data;
 	private $user;
@@ -15,6 +22,9 @@ class User extends Public_Controller {
 		$this->load->library('parser');
 		$this->load->model('user/user_model');
 		$this->user = New User_model;
+
+		$this->load->config('ion_auth', TRUE);
+		$this->identity_column = $this->config->item('identity', 'ion_auth');
 	}
 
 
@@ -116,6 +126,136 @@ class User extends Public_Controller {
 		redirect('user/login');
 	}
 
+	/**
+	 * Allows an admin user to login as another user.
+	 * @return type
+	 */
+	public function login_as_user()
+	{
+		// Only Admins can acccess this function.
+		if (!$this->session->is_admin)
+		{
+			redirect('/', 'refresh');
+		}
+		
+		// Get the user_id from the URL
+		$user_id = $this->uri->segment(3);
+
+		// Get the user record from ion_auth
+		$user = $this->ion_auth->user($user_id)->row();
+
+		// If we don't have a valid $user redirect back to client list
+		if (!$user) {
+			redirect('/user/user_list','refresh');
+		}
+
+		// If the user you are trying to login as is not active abort.
+		if ($user->active == 0)
+		{
+			$this->session->set_flashdata('error', 'That user is not active,  you cannot login is an inactive user.');
+			redirect('/user/user_list','refresh');
+		}
+		//Store the new users data.
+		$session_data = array(
+			'identity'				=> $user->{$this->identity_column},
+			$this->identity_column	=> $user->{$this->identity_column},
+			'email'					=> $user->email,
+			'user_id'				=> $user->id, //everyone likes to overwrite id so we'll use user_id
+			'old_last_login'		=> $user->last_login
+			);
+
+		$this->session->set_userdata($session_data);
+
+		//Set the new users admin status.
+		if($this->ion_auth->is_admin()) 
+		{
+			$this->session->set_userdata('is_admin', TRUE);
+			redirect('/user/user_list','refresh');
+		} 	else {
+			$this->session->set_userdata('is_admin', FALSE);
+			redirect('/user/user_list','refresh');
+		}
+
+		echo "Logging in as $user->first_name";
+
+	}
+
+	/**
+	 * Allows an admin user to toggle the status of a user from suspend to active and visa versa
+	 * @return type
+	 */
+	public function toggle_status()
+	{
+		if (!$this->session->is_admin)
+		{
+			redirect('/', 'refresh');
+		}
+
+		$user_id = $this->uri->segment(3);
+		$user = $this->ion_auth->user($user_id)->row();
+		$status = $user->active;
+		
+		if ($status) {
+			$status = 0;
+		} else
+		{
+			$status = 1;
+		}
+
+		$data = array(
+			'active' => $status,
+			);
+		
+		$this->ion_auth->update($user_id, $data);
+
+		/* CONVERT THIS ONCE ASSISTANT MODULE IN PLACE.
+		//  If status is 0 then suspend all assistants also.
+		if (!$status) {
+			# Acccess assistant model
+			$assistant = new Assistant_model;
+			// Get all Assistants belonging to the suspended user
+			// If user we are suspending is not an owner then the return will be false.
+			$result = $assistant->get_all($user_id);
+			$users = $result['results'];
+
+			if ($users) 
+			{
+				foreach ($users as $user) 
+				{
+					$user_id = $user['user_id'];
+					$this->ion_auth->update($user_id, $data);
+				}
+			}
+		} */
+
+		redirect('/user/user_list','refresh');
+	}
+
+	/**
+	 * Allows an admin user to edit any user on the system.
+	 * @return type
+	 */
+	public function user_edit()
+	{
+		if (!$this->session->is_admin)
+		{
+			redirect('/', 'refresh');
+		}
+	}
+
+	public function display_session()
+	{
+		if (!$this->session->is_admin)
+		{
+			redirect('/', 'refresh');
+		}
+		echo "<pre>";
+		echo "Session data <br />";
+		print_r ($this->session->all_userdata());
+		echo "</pre>";
+		
+		
+	}
 
 
 }
--- application/modules/user/views/user_list_view.php
+++ application/modules/user/views/user_list_view.php
@@ -7,23 +7,32 @@
 			</div>
 			<!-- /.panel-heading -->
 			<div class="panel-body">
-				<table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+				<table width="100%" class="table table-striped table-bordered table-hover" id="users-list">
 					<thead>
 						<tr>
+							<th>ID</th>
 							<th>User Name</th>
 							<th>email</th>
 							<th>First Name</th>
 							<th>Last Name </th>
 							<th>Company</th>
+							<th>Actions</th>
 						</tr>
 					</thead>
 					{list}<tbody>
 					<tr class="odd gradeX">
+						<td>{id}</td>
 						<td>{username}</td>
 						<td>{email}</td>
 						<td>{first_name}</td>
 						<td>{last_name}</td>
 						<td>{company}</td>
+						<td class="text-center"> <div class="btn-group">
+							<a href="/user/login_as_user/{id}" class="btn btn-info btn-xs" title="Login as this user"><span class="glyphicon glyphicon-user"></span></a>
+							<a href="/user/toggle_status/{id}" class="btn btn-success btn-xs" title="Suspend/Unsuspend"><span class="glyphicon glyphicon-pause"></span></a>
+							<a href="/user/user_edit/{id}" class="btn btn-info btn-xs" title="Edit"><span class="glyphicon glyphicon-pencil"></span></a>
+							<a href="#modal-{id}" data-toggle="modal" class="btn btn-danger btn-xs" title="Delete"><span class="glyphicon glyphicon-trash"></span></a>
+						</div></td>
 					</tr>
 					{/list}
 				</tbody>
@@ -37,8 +46,9 @@
 
 <script>
 	$(document).ready(function() {
-		$('#dataTables-example').DataTable({
+		$('#users-list').DataTable({
 			responsive: true
 		});
 	});
 </script>
+
